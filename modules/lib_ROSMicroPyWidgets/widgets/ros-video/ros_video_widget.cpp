ebbe423c2431fefd2450b31f9a759e0b292d9813
John Gentilin <gentijo@gmail.com>
2024-11-01 19:41:41 -0700

    wip checkin

--- modules/lib_ROSMicroPyWidgets/widgets/ros-video/ros_video_widget.cpp
+++ modules/lib_ROSMicroPyWidgets/widgets/ros-video/ros_video_widget.cpp
@@ -51,30 +51,25 @@ void ros_video_service_callback( const void *response, void *context) {
 ros_video_widget::ros_video_widget() {
 };
 
+/**
+ * @brief Constructor for the ros_video_widget class
+ * 
+ * @param parent The parent LVGL object
+ */
 ros_video_widget::ros_video_widget(mpy_LvObject* parent) {
 
-  // Set the quality of service
-//   rclcpp::QoS video_qos(10);
-//   video_qos.keep_last(10);
-//   video_qos.best_effort();
-//   video_qos.durability_volatile();  
-//   // Subscribe to image topics
-//   auto subscription_ = g_node->create_subscription<sensor_msgs::msg::Image>(
-//        "image", video_qos, image_callback);
 
-
+    /**
+     * @brief Create message memory
+     * 
+     * This function creates memory for the CompressedImage message. It also
+     * configures the memory allocation rules for the message.
+     */
     int rc;
-   
     static micro_ros_utilities_memory_conf_t conf = {};
-
-    // OPTIONALLY this struct can configure the default size of strings, basic sequences and composed sequences
-
     conf.max_string_capacity = 50;
     conf.max_ros2_type_sequence_capacity = 15000;
     conf.max_basic_type_sequence_capacity = 15000;
-
-    // OPTIONALLY this struct can store rules for specific members
-    // !! Using the API with rules will use dynamic memory allocations for handling strings !!
 
     static micro_ros_utilities_memory_rule_t rules[] = {
         {"header.frame_id", 30},
@@ -83,41 +78,56 @@ ros_video_widget::ros_video_widget(mpy_LvObject* parent) {
     conf.rules = rules;
     conf.n_rules = sizeof(rules) / sizeof(rules[0]);
 
-    printf("Create Message Memory\r\n");
+    // Create message memory
     micro_ros_utilities_create_message_memory(
-      ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage),
-      &img_msg,
-      conf);
+        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage),
+        &img_msg,
+        conf);
 
-    printf("RosVideo create sub\r\n");
-	RCCHECK(rclc_subscription_init_default(
-	    &rcl_subscription,
-		&rmp_rcl_node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage), 
+    // Set the quality of service
+    rclcpp::QoS video_qos(10);
+    video_qos.keep_last(1);
+    video_qos.best_effort();
+    video_qos.durability_volatile();  
+
+    // Subscribe to image topics
+    RCCHECK (rclc_subscription_init(
+        &rcl_subscription,
+        &rmp_rcl_node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage),
+        topicName, video_qos));
+    );
+   
+    // Create rcl subscription
+    RCCHECK(rclc_subscription_init_default(
+        &rcl_subscription,
+        &rmp_rcl_node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage),
         topicName)
-	);
+    );
 
-
-    printf("RosVideo add sub\r\n");
-	RCCHECK(rclc_executor_add_subscription_with_context(
-		&rmp_rclc_executor,
-		&rcl_subscription,
-	    &img_msg,
-		&ros_video_service_callback, 
+    // Add subscription to the executor
+    RCCHECK(rclc_executor_add_subscription_with_context(
+        &rmp_rclc_executor,
+        &rcl_subscription,
+        &img_msg,
+        &ros_video_service_callback,
         this,ON_NEW_DATA));
 
-    printf("ROS create img\r\n");
+    // Create an LVGL image object
     lvObject = lv_img_create(parent->lvObject);
     lv_obj_set_size(lvObject, lv_pct(100), lv_pct(100));
-    lv_obj_clear_flag(lvObject, LV_OBJ_FLAG_SCROLLABLE); 
+    lv_obj_clear_flag(lvObject, LV_OBJ_FLAG_SCROLLABLE);
 
+    // Configure the image object
     //img_dsc.header.always_zero = 0;
     //img_dsc.header.w = LV_IMG_CF_UNKNOWN;
     //img_dsc.header.h = LV_IMG_CF_UNKNOWN;
-    //img_dsc.header.cf = LV_IMG_CF_RAW;  
+    //img_dsc.header.cf = LV_IMG_CF_RAW;
 
     printf("ROS Video init complete\r\n");
-    
+
+    // Configure the littlefs filesystem
     const esp_vfs_littlefs_conf_t lfsconf = {
         .base_path = "/littlefs",
         .partition_label = "vfs",
@@ -137,7 +147,7 @@ ros_video_widget::ros_video_widget(mpy_LvObject* parent) {
         } else {
             printf("Failed to initialize LittleFS (%s)", esp_err_to_name(ret));
         }
-    }   
+    }
 
     size_t total = 0, used = 0;
     ret = esp_littlefs_info(lfsconf.partition_label, &total, &used);
